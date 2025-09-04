29a0500942347f08001b15089022194de3e3e2d8
ANURAG2428 <anurag28kamboj@gmail.com>
2025-09-04 17:28:39 +0530

    Time: 19 ms (6.04%) | Memory: 14.4 MB (12.46%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/* 
+t.c = O(n) // kyoki hme puri LL traverse krke to dekhni pd hi rhi hai
+s.c = o(n) // kyoki hmne unordered_Set create kara hai aur usme unique address ke node store karaynge 
+ */
+
+
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*>set; // store unique addresses (key = addresses)
+        ListNode* temp = head; //hmne ek temp bna diya jo ki same head ko point karega , taaki jo bhi change's ho temp mai ho na ki head mai
+        while(temp!=NULL){
+            // means agr mera iteratior end() ko point nhi kr rha means's there is a cycle in the linked list
+            if(set.find(temp)!=set.end()){
+                return true;
+            }
+            // aur agr temp ka iterator == end ke baad position tk pahunch gya means , its a unique node , mtlb is node ko kisi ka next wapis point nhi kr rha , so ise store kara lo
+            set.insert(temp); // store this node in unordered_set
+            temp = temp->next; // temp ko aage bda lo
+            
+        }
+        return false;       
+    }
+};
