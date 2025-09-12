c8eea1795560b409352b2fff01cd517f9ec01b7e
ANURAG2428 <anurag28kamboj@gmail.com>
2025-09-12 22:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.07%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -8,44 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/* 
+Optimized Approch : 1) here use two pointer slow and fast (both pointing to head initially)
+                    2) ab fast pointer ko n(given in que) tk traverse kara using for loop
+                    3) jese hi fast n pai pahunch jaye ab slow aur fast ko by 1 step traverse kara jb tk fast->next!=NULL
+                    4) ab jese hi loop end hoga mera slow pointer will be just before the node jo delete krna hai , then i will simply create a pointer that will point to the node that need's to be deleted 
+                    5) mai slow ke next ko waha point kara dunga jaha deleted node ka next point kr rha hai 
+                    6) then i will delete the deleted node using delete keyword and return the head
+
+NOTE-  this code won't work for deleting head node, so we have to write EDGE CASE for it   
+ */
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // first Calculate the length of LL
-        ListNode* temp = head;
-        int len = 0;
-        while(temp!=NULL){
-            len++;
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // ListNode* temp = head;
+        // STEP 1: traverse fast pointer till n (kyoki mujhe jo node delete krna hai us se just pehle wale node tk reach krna hai)
+        for(int i = 0; i<n ;i++){ // if i<=n , to fast aage nikal jayega
+            fast = fast -> next;
         }
-        // loop end -> means hame length of LL pta chal gyi hogi
+        // now my fast will be just before the n = 2 from the end position
 
-        
-        
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        int cnt = 0;
-        int backcount = len-n;
-        // If we need to remove the head (backcount == 0) (len-n = 2-2(2nd position from the last of LL))
-        if(backcount == 0) {
-            ListNode* newHead = head->next;
-            delete head; // Optional: free memory
-            return newHead;
+        // STEP 2 : ab slow and fast ko by 1 step simultenously move kra jb tk fast -> next != NULL
+        if(fast == NULL){ // EDGE CASE
+            
+            return head->next; // mean's head ke just aage wale node se list print kara do
         }
-        
-        while(cnt!=backcount){
-            prev = curr;
-            curr = curr->next;
-            cnt++;
+        while(fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next;
         }
-        // loop end hote hi , curr will be at the node jo delete krna hai and prev will at (node-1)
-        prev->next = curr->next; // jaha mera curr pehle , point kr rha tha waha ab prev point karega
-        curr->next = NULL; // curr ke next ko NULL kar diya
-        delete curr;
+
+        // STEP 3: jo node delete krna hai use kisi pointer mai store kara de
+        ListNode* deleteNode = slow->next;
+        slow->next = deleteNode->next;
+        delete deleteNode;
 
         return head;
 
     }
-    
-    
 };
