a902b7cf708df2854649901e0e716f633c765150
ANURAG2428 <anurag28kamboj@gmail.com>
2025-09-24 22:50:48 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (3.12%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-     int mod = 1e9 + 7;
+    int mod = 1e9 + 7;
     
     long long power(long long x, long long y) {
         long long res = 1;
@@ -17,13 +17,12 @@ public:
     }
     
     int countGoodNumbers(long long n) {
-        long long even_count = (n + 1) / 2; // Even indices count
-        long long odd_count = n / 2;         // Odd indices count
+        long long even_count = (n + 1) / 2;
+        long long odd_count = n / 2;
         
         long long even_ways = power(5, even_count);
         long long odd_ways = power(4, odd_count);
         
         return (even_ways * odd_ways) % mod;
-        
     }
 };
