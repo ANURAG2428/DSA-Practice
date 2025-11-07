ada6e59de3fc3cb518c6809fa73755d7eb29167f
ANURAG2428 <anurag28kamboj@gmail.com>
2025-11-07 18:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.71%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-         int diff = start ^ goal; // XOR karo, jahan 1s hain, wo positions hain jahan bit flip karni padegi
-        int flips = 0;
+        int ans = start ^ goal;
 
-        // Jab tak diff mein koi bit bachi hai, loop chalao
-        while (diff) {
-            flips += (diff & 1);  // Agar current bit 1 hai, flips ko increment karo
-            diff >>= 1;           // Diff ko right shift karo
+        int cnt = 0;
+        // apply count function for calculating set bit in ans
+        while(ans!=0){
+            ans = ans & (ans-1);
+            cnt = cnt + 1; 
+
         }
-
-        return flips;  // Final number of flips return karo
+        return cnt;
+        
     }
-    
 };
