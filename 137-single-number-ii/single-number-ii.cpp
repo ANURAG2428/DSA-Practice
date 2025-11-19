4928f5d3667b1dd9847fa898f7ebb4b2a397b46f
ANURAG2428 <anurag28kamboj@gmail.com>
2025-11-19 13:37:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (98.61%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
-        // Har bit (0 se 31) tak check karenge
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
-            for (int num : nums) {
-                // Agar i-th bit set hai, toh count badhao
-                if (num & (1 << i)) {
-                    sum++;
-                }
-            }
-            // Agar ye bit 3 se divide nahi hoti,
-            // iska matlab ye bit unique number me present hai
-            if (sum % 3 != 0) {
-                result |= (1 << i);
-            }
-        }
-        return result;
+           int ones = 0, twos = 0;
+    
+    for(int num : nums) {
+        // Step 1: Update 'ones'
+        // XOR with num: agar bit pehli baar aayi toh ones mein set ho
+        // & ~twos: ensure ki jo bit already twos mein hai, wo ones mein na aaye
+        ones = (ones ^ num) & ~twos;
+        
+        // Step 2: Update 'twos' 
+        // XOR with num: agar bit dusri baar aayi toh twos mein set ho
+        // & ~ones: ensure ki jo bit ones mein set ho gayi, wo twos mein na aaye
+        twos = (twos ^ num) & ~ones;
+    }
+    
+    return ones; // Final answer ones mein hoga
     }
 };
