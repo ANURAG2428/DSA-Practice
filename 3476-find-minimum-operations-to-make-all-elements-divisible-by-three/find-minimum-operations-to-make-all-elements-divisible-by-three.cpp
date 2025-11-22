db4bd178dbdaa8cf4dfb469619528e486aa9a0be
ANURAG2428 <anurag28kamboj@gmail.com>
2025-11-22 22:16:40 +0530

    Time: 2 ms (3.70%) | Memory: 23.2 MB (79.62%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int cnt = 0; 
-        for(auto num : nums ){
-            if((num - 1) % 3 == 0) cnt++;
-            else if ((num + 1) % 3 == 0) cnt++;
+        int cnt = 0;
+        // here if have to return the minimum no of operations to make all elements of nums divisible by 3
+        // Note - yaha mujhe no operation return krne hai kese 3 se divisble kareynege uski fikar nhi krni hai
 
+        for(auto num : nums){
+            if(num % 3 == 1) cnt++; // means mujhe ek operation perform krna hai that is substract (-1)
+            else if (num % 3 == 2) cnt++; // means mujhe ek operation perform krna hai that is substract (+1)
+            // if(num % 3 == 0) , means i don't need any operation
         }
-        if(cnt == 0) return 0;
         return cnt;
     }
 };
