9eab24c1dbceb2dad8d5a47d0c7278bf696c7b32
ANURAG2428 <anurag28kamboj@gmail.com>
2026-02-17 22:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.59%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,44 +1,56 @@
-#include <stack>
-using namespace std;
+// Approch 1 : ye mai tb implement kar rha hu , jb push operations km hai , kyoki mere push ke code mai 2 while loop honge which makes its T.C = O(2N) and it is a costly operation and will reduce my code effeciency and perfomance
 
 class MyQueue {
+    private:
+    stack<int>st1;  // stack 1 
+    stack<int>st2;  // stack 2
 public:
-    stack<int> s1, s2; // Do stacks use karenge
-
     MyQueue() {
-        // Constructor - kuch initialize nahi karna
+        // It is default constructor that is called when object is created of the above class , 
     }
     
-    // Push operation - element ko queue ke end me dalna
-    void push(int x) {
-        // Step 1: sab kuch s1 se s2 me daal do (reverse karne ke liye)
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
+    void push(int x) {  // T.C = O(2n) , this code of push is only applicable if push operations are way less as compare to pop() and top() , else we prefer to follow Approch 2 , where large no of push operation are handeled with effective T.C
+
+
+        // step 1 : st1 ke sare element st2 mai daal do 
+        while(st1.size()){
+            st2.push(st1.top());
+            st1.pop();
         }
-        // Step 2: naya element s1 me daal do
-        s1.push(x);
-        // Step 3: wapas s2 ke elements ko s1 me daal do
-        while (!s2.empty()) {
-            s1.push(s2.top());
-            s2.pop();
+
+        // Step 2 : ab st1 mai new element x daal do
+        st1.push(x);
+
+        // step 3 : st2 ke sare element st1 mai wapis daal do
+        while(st2.size()){
+            st1.push(st2.top());
+            st2.pop();
         }
+
+        // here push operation is handeled fully 
     }
     
-    // Pop operation - front element ko remove karna
     int pop() {
-        int topElement = s1.top();
-        s1.pop();
-        return topElement;
+        int n = st1.top();
+        st1.pop();
+        return n;
     }
     
-    // Peek operation - front element dekhna (remove nahi karna)
     int peek() {
-        return s1.top();
+        return st1.top();
     }
     
-    // Empty check karna
     bool empty() {
-        return s1.empty();
+        return st1.empty();
+        
     }
 };
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
