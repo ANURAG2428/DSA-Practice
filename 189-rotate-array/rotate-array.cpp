ebab98793b0637622fdcc18fa11a67d72886bc88
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-04-16 02:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (66.20%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
+    void SwapFunc(vector<int>&nums , int n , int i , int j){
+        while(i<j){
+            swap(nums[i] , nums[j]);
+            i++;
+            j--;
+        }   
+    }
+
     void rotate(vector<int>& nums, int k) {
-        // By using Extra Space
         int n = nums.size();
-        vector<int>arr(n);
-        // if k is greater than n , in this case also use % operator
-        // k = k % n;
-        for(int i = 0; i<n ; i++){
-            arr[(i+k) % n] = nums[i];
-        }
-        
-        nums = arr;
+
+        // if k is greater than  n
+        k = k % n;
+        if(k == 0) return; // means no need to rotate
+
+        // Step1 : Reverse the whole array
+        SwapFunc(nums , n , 0 , n-1);
+        // Step 2 : Reverse the 1st k elements
+        SwapFunc(nums , n , 0 , k-1 );
+        // Step 3 : Reverse the last n-k elmenets
+        SwapFunc(nums , n , k , n-1 );
+
         
     }
 };
