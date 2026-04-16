e56dda4aa66ecee2d272e49e86c0f34f3c889d37
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-04-16 15:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (24.34%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       int result = 0;
-       for(int i = 0 ; i<nums.size() ; i++){
-            result = result ^ nums[i];
-       }
-       return result;
+        int n = nums.size();
+
+        int xor1 = 0;
+        for(int i = 0; i<n; i++){
+            xor1 = xor1 ^ nums[i];
+        }
+
+        return xor1;
     }
 };
