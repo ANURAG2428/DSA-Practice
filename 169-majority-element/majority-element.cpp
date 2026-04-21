4ee2cafc34a93365b590c53fe1ada04d0eabb8ee
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-04-21 12:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (63.38%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt = 0 ;
-        int element ;
-        for(int i = 0 ; i<nums.size() ; i++){
-            if(cnt== 0){
-                element = nums[i];
-                cnt = 1;
-            }
-            else if(nums[i] == element){
+        int n = nums.size();
+        int cnt = 0;
+
+        int el = nums[0];
+        cnt = 1;
+        for(int i = 1; i<n ; i++){
+            if(nums[i] == el){
                 cnt++;
             }
             else{
                 cnt--;
+                }
+            if(cnt == 0){
+                el = nums[i];
+                cnt = 1;
             }
         }
 
-        // Now check kr ki majority element jo mila hai numso n/2 se jada time present hai
+        // Now loop se check kr el element mai present element  > n/2 present hai
         int newcnt = 0;
-        for(int i = 0; i<nums.size() ; i++){
-            if(nums[i] == element){
+        for(int i = 0; i<n ; i++){
+            if(nums[i] == el){
                 newcnt++;
             }
-        }
-        if(newcnt > nums.size()/2 ){
-            return element;
-        }
-        return -1;
+            if(newcnt > n/2){
+                return el;
+            }
+        } 
 
+        return -1;
     }
 };
