fb1858dd020ebbc8ec8159e94067d6c91f0e5d65
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-07-02 13:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (89.34%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,45 +1,54 @@
 class Solution {
 public:
-    // Largest index where arr[mid] <= target
-    int floorIndex(vector<int>& arr, int target) {
-        int s = 0, e = arr.size() - 1;
-        int ans = -1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (arr[mid] <= target) {
-                ans = mid;
-                s = mid + 1;   // right side for larger <= target
-            } else {
-                e = mid - 1;
-            }
+    
+// This function will help in getting the first occurence of target element in sorted vector
+int lowerBound(vector<int>&v, int n, int target){
+    int ans = n; // by default agr target element nhi mila to ye hypothetical index return krta hai -> which is out of array means n index (Size of array)
+    int s = 0 , e = n-1;
+    while(s<=e){
+        int mid = s + (e-s)/2;
+        if(v[mid] >= target){
+            // agr meri lower bound ki condition true hogai -> will look in left side to get the most optimal element which is greater than or equal to target element
+            ans = mid;  // ans me maine current lower bound index store kara diya (bcz i hit the lower bound check condition)
+            e = mid -1;
         }
-        return ans;
+        else{
+            s = mid +1;
+        }
     }
+    return ans;
+}
+ 
 
-    // Smallest index where arr[mid] >= target
-    int ceilIndex(vector<int>& arr, int target) {
-        int s = 0, e = arr.size() - 1;
-        int ans = -1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (arr[mid] >= target) {
-                ans = mid;
-                e = mid - 1;   // left side for smaller >= target
-            } else {
-                s = mid + 1;
-            }
+// This function will help to get the last occurence of target element kyoki upper bound function return index of arr[i]>target , means here arr[i] is -> the smallest element which is greater than target -> so it will return the index of the smallest element greater than target element 
+int UpperBound(vector<int>&v, int n, int target){
+    int ans = n; // agr target element last element hai sorted array ka so it will return the hypothetical index which -> is the n (which is size of array)
+    int s = 0 , e = n-1;
+    while(s<=e){
+        int mid = s + (e-s)/2;
+        if(v[mid]>target){  
+            // means upper bound ki condition hit ho gai -> so i will look in left side to get the index of smallest element which is greater than the target element
+            ans = mid;
+            e = mid -1;
         }
-        return ans;
+        else{
+            s = mid + 1;
+        }
     }
+    return ans;
+    
+} 
 
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = ceilIndex(nums, target);
-        int last  = floorIndex(nums, target);
 
-        // Agar target exist nahi karta
-        if (first == -1 || last == -1 || nums[first] != target || nums[last] != target) {
-            return {-1, -1};
-        }
-        return {first, last};
+    vector<int> searchRange(vector<int>& v, int target) {
+        int n = v.size();
+        int first = lowerBound(v , n , target);
+    // means agr lowerbound n return krta hai means target element present hi nhi hai vector hai + ya jo first index mila vo waha target element hai hi nhi then also first occurence condition fails -> will simply return  {-1,-1}
+    if(first == n || v[first] != target ) return {-1,-1}; //is case mai second dhundhne ki jarurat hi nhi will simply return -1,-1
+
+
+    // agr upr ki if condition fails hoti hai will simply return {lowerbound , upperbound}
+    int last = UpperBound(v , n , target);
+    return {first  , last -1 };      
     }
 };
