c052c83a91f07f5d8589d0c681dabc4b2dc81579
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-07-24 14:37:07 +0530

    Time: 20 ms (5.42%) | Memory: 49.3 MB (5.34%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,56 +2,30 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n = s.size();
-        // Step 1 : trim the string from both starting and ending end via 2 d/f
-        // while loops
-        int i = 0;
-        int j = n - 1; // represents the last character
-        while (i <= j && s[i] == ' ')
-            i++;
-        while (j >= i && s[j] == ' ')
-            j--;
-        // Step 2 : extract the string which does not have trailing space at the
-        // start and end
-        s = s.substr(i,
-                     j - i + 1); // +1 because -> we have used 0 based indexing
+        // Step 1 : input string ko reverse kro
+        reverse(s.begin(), s.end());
+        // step 2 : create an ans string which will have final reversed word
+        // string
+        string ans = "";
 
-        // Step 3 : ab after extracting above string -> ab mai iska each word ek
-        // string vector mai store karaunga
-        vector<string> words;
-        string word = "";
-        // now ab mai simply each word nikalunga aur usko vector mai dalunga
-        for (auto c : s) {
-            if (c != ' ') {
-                word = word + c;
-            } else {
-                if (!word.empty()) { // this will prevent from storing empty
-                                     // string in vector
-                    words.push_back(word);
-                    word = ""; // word ko dubara empty string kr do
-                }
+        // Step 3 : simply ab reversed string pai iterate kr + is for loop se "
+        // " spaces automatically ignore ho jayenge and each word -> each time
+        // would get stored in string word and will reverse that and store in
+        // ans
+        for (int i = 0; i < n; i++) {
+            string word = "";
+            while (i < n && s[i] != ' ') {
+                word = word + s[i];
+                i++;
+            }
+
+            // now ab jo word mila hai usse reverse kr de
+            reverse(word.begin(), word.end());
+            if (word != "") {
+                ans = ans + " " + word;
             }
         }
-
-        // Note - last word kabhi vector of string mai jayega nhi bcz last
-        // iteration ke baad space hai hi nhi so else kabhi execute nhi hoga so
-        // we have to store it in vector manually
-        if (!word.empty()) { // word mai last word present hoga so this if will
-                             // add that
-            words.push_back(word);
-        }
-
-        string result = "";
-
-        // Step 4 : ab string vector mai -> mene sare words store kara rakhe hai
-        // -> so ab vector ko piche se travese kr aur hr word ke result which is
-        // new string usme store kara dunga  hone ke baad space print kr execept
-        // 0th index word ko chor ke
-        for (int i = words.size() - 1; i >= 0; i--) {
-            result += words[i];
-            if (i != 0) {
-                result += " ";
-            }
-        }
-        return result;
+        return ans.substr(
+            1); // means 1st index se puri string a subsstring return krdo
     }
 };
