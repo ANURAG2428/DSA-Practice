9df616288828062b57a3aa8c1108e508106eeb62
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-07-27 02:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.size();
+        int n1 = s.size();
         int n2 = t.size();
-        vector<int>hashvec(26 , 0);
 
-        for(int i = 0 ; i< n ; i++){
-            hashvec[s[i] - 'a']++;
+        // Edge Case : if both string ki length hi same nhi hai so return false
+        // kra de pehle hi
+        if (n1 != n2)
+            return false;
+
+        // create a vector of char type which will store freq of each char
+        vector<int> v(26, 0); // means declare a vector of size 26 and all
+                              // element are initialized with 0
+
+        for (int i = 0; i < n1; i++) {
+            int s1 = s[i] -
+                     'a'; // ye index nikal rha hai so ye int type ka hi to hoga
+            v[s1]++;
+            int t1 =
+                t[i] -
+                'a'; // // ye index nikal rha hai so ye int type ka hi to hoga
+            v[t1]--;
         }
 
-        // now t string iterate kr agr hashvec pura 0 hogya means its a anagram
-        for(int i = 0; i<n2 ; i++){
-            hashvec[t[i] - 'a']--;
-        }
-
-        for(int i = 0; i<26 ; i++){
-            if(hashvec[i] != 0){
+        for (int i = 0; i < 26; i++) {
+            if (v[i] > 0)
                 return false;
-            }
         }
         return true;
     }
