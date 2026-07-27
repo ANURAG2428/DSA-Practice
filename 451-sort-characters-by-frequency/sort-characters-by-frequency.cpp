8d244171bd8d1e8dea252e40d146640c44683d36
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-07-27 17:23:15 +0530

    Time: 4 ms (37.23%) | Memory: 11.1 MB (54.71%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     string frequencySort(string s) {
         int n = s.size();
-        // Step 1: create an unordered_map
-        unordered_map<char, int> mpp;
-        // step 2: store each char of string with its freq in each iteration
-        // using loop
+        // Step 1:create a vector of size 128
+        vector<int> freq(128, 0);
         for (int i = 0; i < n; i++) {
-            mpp[s[i]]++;
+            freq[s[i]]++; // so it will increase the count at s[i] index means
+                          // if s[i] = 'a' -> so a in int is 97 ASCII value , so
+                          // it will increase freq count there
         }
 
-        // // step 3 : ab mai mpp(char, freq(int)) ko vector of size 62 mai
-        // dalunga and us vector ko dec order on the basis of freq sort  dunga
-        // vector<pair<char,int>>v(62);
-        // // put all map element with its freq in vector
-        // Step 2: Map ko vector of pairs mein daalo
-        vector<pair<char, int>> v(mpp.begin(),
-                                  mpp.end()); // 🔥 FIX 1: Dynamic siz
+        // Step 2 : ab mai ek vector banaunga of pair type consisting of char ,
+        // int -> which will store freq of each char in each itreation
+        vector<pair<char, int>> v;
+        for (int i = 0; i < freq.size(); i++) {
+            if (freq[i] > 0) { // means element present
+                // now put the char and freq in this pair vector v
+                v.push_back({(char)i, freq[i]});
+
+                // or we can also write it as , below
+                /*
+                pair<char,int> p;
+
+                p.first = (char)i;
+                p.second = freq[i];
+
+                v.push_back(p);
+                */
+            }
+        }
 
         // now ab vector mai sare {char, freq} store ho gye -> now sort them in
         // decreasing order on the basis of their freq
@@ -28,7 +40,7 @@ public:
         // -> so ab ek new string mai ye char store kara de freq wise
         string ans = "";
         for (auto& p : v) {
-            // 🔥 FIX 2 & 3: Char ko 'freq' times append kar
+
             ans.append(p.second,
                        p.first); // C++ string constructor: append(count, char)
             // Ya manually: for(int i=0; i<p.second; i++) ans += p.first;
