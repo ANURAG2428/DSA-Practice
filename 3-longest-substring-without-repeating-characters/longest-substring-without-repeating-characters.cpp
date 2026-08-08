0d1a87a356b74ec7d6eb8cf732e9776149157644
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-08-08 13:45:37 +0530

    Time: 12 ms (46.21%) | Memory: 19.2 MB (14.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -10,33 +10,17 @@ public:
             return 1;
 
         int maxlen = 0;
-        // Step 1: sare substring generate kr using nested loop
-        for (int i = 0; i < n; i++) {
-            vector<int> v(256, 0); // constant size ka vector allocate kara rhe
-                                   // hai for each substring where each value at
-                                   // every index is initialized with 0
-
-            // NOTE - now mai chahta hu ki kisi bhi vector ke index pai value >
-            // 1 nhi honi chahiye , so iteration mai index pai value increase
-            // karate hue kese karu check ki hr char jo substring ka mila hai
-            // uski value at its index ==1 , not greater than 1
-            for (int j = i; j < n; j++) {
-                // NOTE - Here hr j loop ki iteration mai substring hi bn rhi
-                // hai
-                if (v[s[j]] > 0) {
-                    break; // means already is character ke ascii value index
-                           // pai iska cnt = 1 pehle ho chuka hai , means is
-                           // substring mai ye character ek baar pehle aa chuka
-                           // hai , so ye substring ko hm consider nhi kr skte
-                           // to get the length of the
-                           // longestNonRepeatingSubstringLength
-                }
-                // agr above condition true nhi hai then it means ye char pehle
-                // baar aaya hai is substring mai and mai iska index pai value 1
-                // kr dunga
-                v[s[j]]++;
-                maxlen = max(maxlen, j - i + 1);
+        int st = 0, e = 0;
+        vector<int> v(256, 0);
+        while (e < n) {
+            while(v[s[e]] >=
+                1) { // means mere current substring mai dupicate element hai
+                v[s[st]]--;
+                st++;
             }
+            v[s[e]]++;
+            maxlen = max(maxlen, e - st + 1);
+            e++;
         }
         return maxlen;
     }
