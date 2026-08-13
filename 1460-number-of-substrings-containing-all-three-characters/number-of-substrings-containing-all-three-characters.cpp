7c35e948863f9dece09b322ba5c6e10b20023ade
Anurag Kamboj <anurag28kamboj@gmail.com>
2026-08-13 22:58:18 +0530

    Time: 1 ms (93.35%) | Memory: 11.5 MB (15.22%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        int n = s.size();
-        int cnt = 0;
-
-        int v[3] = {-1, -1, -1}; // each value is initialized with -1 , as we
-                                 // have to store index in array element
+    long long NoOfSubstringWithAtMost2Char(string s, int n) {
+        long long cnt = 0;
+        int st = 0, e = 0;
         int distinct = 0;
-        int minIndex = INT_MAX;
-        for (int i = 0; i < n; i++) {
-
-            int indx = s[i] - 'a'; // position/index find krna
-
-            if (v[indx] == -1)
-                distinct++; // means 1st time ye char aaya hai iteration mai ,
-                            // not present in v array before
-
-            v[indx] = i; // curr char ko array mai curr iteration index i se
-                         // assign kr diya
-
-            // find min index char from the array storing indexe's
-            // minIndex = min({v[0] , v[1] , v[2]});
-            // or
-            minIndex = min(v[0], min(v[1], v[2]));
-
-            if (distinct == 3) {
-                cnt = cnt + (minIndex + 1);
+        vector<int> v(3, 0);
+        while (e < n) {
+            int indx = s[e] - 'a'; // e pointer pai present character
+            // step 1 : sbse pehle check kr curr char vector mai 1st time aarha
+            // hai -> agr aarha hai to disticnt++ kr de
+            if (v[indx] == 0) {
+                distinct++;
             }
+
+            // step 2 : ab curr char ka cnt vect mai bda de
+            v[indx]++;
+
+            // step 3 : agr disticnt == 3 means 3 chr aagye hai is particular
+            // substring mai
+            while (distinct == 3) {
+                int indxs =
+                    s[st] - 'a'; // starting pointer pai present character
+                v[indxs]--;      // as v stores the cnt of char a,b,c at their
+                                 // particular indexes
+                if (v[indxs] == 0) {
+                    distinct--;
+                }
+                st++;
+            }
+
+            // in case of substring having <=2 char will add it into cnt
+            if (distinct <= 2) {
+                cnt = cnt + (e - st + 1);
+            }
+
+            e++;
         }
 
         return cnt;
+    }
+
+    int numberOfSubstrings(string s) {
+        long long n = s.size();
+        long long total = n * (n + 1) / 2;
+        long long ans = total - NoOfSubstringWithAtMost2Char(s, n);
+
+        return (int)ans;
     }
 };
